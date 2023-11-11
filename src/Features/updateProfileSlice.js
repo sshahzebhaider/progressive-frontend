2c18614b6ff91fb6838d29506a98122d5309a6f5
sshahzebhaider <149874221+sshahzebhaider@users.noreply.github.com>
2023-11-12 00:38:08 +0500

    Update updateProfileSlice.js

--- src/Features/updateProfileSlice.js
+++ src/Features/updateProfileSlice.js
@@ -1,91 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from '../Features/loginSlice';
-import {
-  Alert,
-  AlertIcon,
-  Box,
-  Button,
-  Center,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Spinner,
-} from '@chakra-ui/react';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export const updateUserProfile = createAsyncThunk(
+  'updateUserProfile',
+  async (user, thunkAPI) => {
+    try {
+      const {
+        employeeLogin: { employeeInfo },
+      } = thunkAPI.getState();
 
-  const dispatch = useDispatch();
-  const location = useLocation();
-  const navigate = useNavigate();
-  const redirect = location.search ? location.search.split('=')[1] : '/';
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${employeeInfo.token}`,
+        },
+      };
+      const { data } = await axios.put(`/api/employee/profile`, user, config);
 
-  const employeeLogin = useSelector(store => store.employeeLogin);
-  const { loading, error, employeeInfo } = employeeLogin;
-
-  const handleLogin = e => {
-    e.preventDefault();
-    dispatch(login({ email, password }));
-  };
-
-  useEffect(() => {
-    if (employeeInfo && redirect === '/') {
-      navigate('/dashboard');
-    } else if (employeeInfo && redirect) {
-      navigate(`/${redirect}`);
+      return data;
+    } catch (error) {
+      const newError =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      return thunkAPI.rejectWithValue(newError);
     }
-  }, [employeeInfo, navigate, redirect]);
+  }
+);
 
-  return (
-    <Center height="100vh" bg="#6ea7dc">
-      <Box
-        width="300px"
-        p="6"
-        bg="white"
-        borderRadius="md"
-        boxShadow="md"
-        textAlign="center"
-      >
-        <Heading as="h1" marginBottom="2rem" color="#6ea7dc">
-          Progressive International
-        </Heading>
-        <FormControl marginBottom="1rem">
-          <FormLabel>Email</FormLabel>
-          <Input
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormControl>
-        <FormControl marginBottom="1rem">
-          <FormLabel>Password</FormLabel>
-          <Input
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </FormControl>
-        <Button
-          colorScheme="teal"
-          onClick={handleLogin}
-          marginBottom="1rem"
-          width="100%"
-        >
-          {loading ? <Spinner /> : 'Login'}
-        </Button>
-        {error && (
-          <Alert status="error" borderRadius="md">
-            <AlertIcon />
-            {error}{' '}
-          </Alert>
-        )}
-      </Box>
-    </Center>
-  );
-};
+const initialState = {};
 
-export default LoginScreen;
+export const updateProfileSlice = createSlice({
+  name: 'userLoginReducer',
+  initialState,
+  // reducers: {
+  //   USER_UPDATE_PROFILE_RESET: () => {
+  //     return {};
+  //   },
+  // },
+  extraReducers: {
+    [updateUserProfile.pending]: state => {
+      return {
+        loading: true,
+      };
+    },
+
+    [updateUserProfile.fulfilled]: (state, action) => {
+      return {
+        loading: false,
+        success: true,
+        employeeInfo: action.payload,
+      };
+    },
+
+    [updateUserProfile.rejected]: (state, action) => {
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    },
+  },
+});
+export const { USER_UPDATE_PROFILE_RESET } = updateProfileSlice.actions;
+export default updateProfileSlice.reducer;
